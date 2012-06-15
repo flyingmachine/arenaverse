d6aa6ac014b6bc15845b34fe85ea5d60efdda5a5
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-06-14 21:54:43 -0400

    making my own little routing library

--- src/brainsinvats/views/routes.clj
+++ src/brainsinvats/views/routes.clj
@@ -1,7 +1,22 @@
-(ns brainsinvats.views.routes)
+(ns brainsinvats.views.routes
+  (require [clojure.string :as string]))
 
-(def r '{:arenas/shiny [shiny "/arenas/new"]
-         :arenas/edit  [[:get "/arenas/edit/:id"]]})
+(defn- throwf [msg & args]
+  (throw (Exception. (apply format msg args))))
+
+(def routes '{:arenas/shiny [shiny "/arenas/new"]
+              :arenas/edit  [[:get "/arenas/edit/:id"]]})
+
+(defn url-forr [route-name & [route-args]]
+  (let [route (last (flatten (route-name routes)))
+        route-arg-names (noir.core/route-arguments route)]
+    (when (nil? route)
+      (throwf "missing route for %s" route-name))
+    (when (not (every? #(contains? route-args %) route-arg-names))
+      (throwf "missing route-arg for %s" [route-args route-arg-names]))
+    (reduce (fn [path [k v]]
+              (assert (keyword? k))
+              (string/replace path (str k) (str v))) route route-args)))
 
 (defmacro defpage-r [route & body]
-  `(noir.core/defpage ~@((keyword (str (re-find #"[^.]*$" (str (ns-name *ns*))) "/" route)) r) ~@body))
+  `(noir.core/defpage ~@((keyword (str (re-find #"[^.]*$" (str (ns-name *ns*))) "/" route)) routes) ~@body))
