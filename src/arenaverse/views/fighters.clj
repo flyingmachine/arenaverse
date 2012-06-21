a8fef18f6a1185e1213ba718eabc831086d93b65
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-06-20 21:25:10 -0400

    can update fighters

--- src/arenaverse/views/fighters.clj
+++ src/arenaverse/views/fighters.clj
@@ -2,6 +2,7 @@
   (:require [arenaverse.views.common :as common]
             [arenaverse.models.fighter :as fighter]
             [noir.response :as res]
+            [noir.session :as session]
             [monger.collection :as mc])
   
   (:use noir.core
@@ -12,30 +13,41 @@
 
   (:import [org.bson.types ObjectId]))
 
+(defn redirect-to-arena [fighter]
+  (res/redirect (url-for-r :arenas/show {:_id (:arena-id fighter)})))
+
 (defpage-r create {:as fighter}
   (fighter/create-fighter fighter)
-  (res/redirect (url-for-r :arenas/show {:_id (:arena-id fighter)})))
+  (redirect-to-arena fighter))
 
 (defpartial fighter-fields [{:keys [name bio]}]
   [:tr
    [:td (label :name "Name")]
-   [:td (text-field :name)]]
+   [:td (text-field :name name)]]
   [:tr
    [:td (label :bio "Bio")]
-   [:td (text-field :bio)]]
+   [:td (text-field :bio bio)]]
   [:tr
    [:td (label :file "Pic")]
-   [:tqd (file-upload :file)]])
-
-
+   [:td (file-upload :file)]])
 
 (defpage-r edit {:keys [_id]}
   (let [fighter (mc/find-map-by-id "fighters" (ObjectId. _id))]
     (common/layout
      [:h2 "Editing Fighter: " (:name fighter)]
      (form-to [:post (url-for-r  :fighters/update {:_id _id})]
-            (fighter-fields fighter)
-            [:p (submit-button "Update Arena")]))))
+              [:table
+               (fighter-fields fighter)
+               [:tr
+                [:td]
+                [:td (submit-button "Update Fighter")]]]))))
+
+(defpage-r update {:as fighter}
+  (let [mongo-id (ObjectId. (:_id fighter))
+        record (mc/find-map-by-id "fighters" mongo-id)]
+    (mc/update-by-id "fighters" mongo-id (dissoc (merge record fighter) :_id))
+    (session/flash-put! "Fighter updated!")
+    (redirect-to-arena record)))
 
 (defpartial thumb [record]
   [:div.fighter
