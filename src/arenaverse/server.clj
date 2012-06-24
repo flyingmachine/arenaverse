c72883da3113b13560b50b1395a36f8d67f4fe95
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-06-24 09:12:11 -0400

    adding procfile; mongolab uri

--- src/arenaverse/server.clj
+++ src/arenaverse/server.clj
@@ -10,7 +10,7 @@
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
     (def env mode)
-    (let [uri (get (System/getenv) "MONGODB_URI" (str "mongodb://127.0.0.1/omgsmackdown-" (name mode)))]
+    (let [uri (get (System/getenv) "MONGOLAB_URI" (str "mongodb://127.0.0.1/omgsmackdown-" (name mode)))]
       (monger.core/connect-via-uri! uri))
     (server/start port {:mode mode
                         :ns 'arenaverse})))
