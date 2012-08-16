23bf4da256c76c2bb5331d5eea08ba117dd239ef
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-08-16 10:28:00 -0400

    use clojure 1.4

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject arenaverse "0.1.0-SNAPSHOT"
             :description "wooo!"
-            :dependencies [[org.clojure/clojure "1.3.0"]
+            :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.2.1" :exclusions [org.clojure/clojure]]
                            [com.novemberain/monger "1.0.0-rc1"]
                            [com.cemerick/friend "0.0.9"]
